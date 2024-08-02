d9a92fb984b0da8d52a7df15c57b272ae95d9a77
ciaranoceallaigh <71128211+ciaranoceallaigh96@users.noreply.github.com>
2024-08-02 12:18:57 +0100

    Update set_up.sh

--- set_up.sh
+++ set_up.sh
@@ -1,7 +1,8 @@
 #Start with HapMap_3_r3_1 files from 1_QC_GWAS.zip.
-#cd ~/GWA_tutorial/1_QC_GWAS/
 
-shuf --random-source=<(yes 42) indepSNP.prune.in | head -n 10000 > subset_indep_snps.txt
+cd ~/GWA_tutorial/1_QC_GWAS/
+
+shuf --random-source=<(yes 42) indepSNP.prune.in | head -n 50000 > subset_indep_snps.txt
 sort -g -k 9,9 ../3_Association_GWAS/logistic_results.assoc_2.logistic | awk '{print $2}' | head -n 50 > subset_top_snps.txt
 diff HapMap_3_r3_3.bim HapMap_3_r3_4.bim | grep 'rs' | awk '{print $3}' | shuf --random-source=<(yes 42) | head -n 37 > subset_geno_0.02.txt
 diff HapMap_3_r3_8.bim HapMap_3_r3_7.bim | grep 'rs' | awk '{print $3}' | shuf --random-source=<(yes 42) | head -n 89 > subset_maf_0.05.txt
